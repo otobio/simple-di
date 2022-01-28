b76176904e866799fe83187c0450325789ab1b5b
Ojutiku Oluwatobi <xtobsmagic@aol.com>
2022-01-28 21:46:06 +0400

    Initial simple DI implemenntation

---
+++ src/SimpleDI.php
@@ -0,0 +1,162 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Otobio;
+
+use Closure,
+    ReflectionClass,
+    ReflectionMethod,
+    ReflectionException,
+    Exception;
+
+use Psr\Container\ContainerInterface;
+
+class SimpleDI implements ContainerInterface {
+
+    protected
+        $sharedInstances = [],
+        $bindings = [];
+
+
+    public function getBinding(string $id)
+    {
+        while (true) {
+            if (isset($this->bindings[$id])) {
+                $binding = $this->bindings[$id];
+                if (is_string($binding['concrete']) && array_key_exists($binding['concrete'], $this->bindings)) {
+                    $id = $binding['concrete'];
+                    continue;
+                }
+
+                return $binding;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    protected function isShared(string $id)
+    {
+        $binding = $this->getBinding($id);
+        return isset($binding) && $binding['shared'] === true;
+    }
+
+
+    protected function clearResolved(string $id)
+    {
+        unset(
+            $this->bindings[$id],
+            $this->sharedInstances[$id]
+        );
+    }
+
+    public function add(string $id, array $configuration = [])
+    {
+        $this->clearResolved($id);
+
+        $this->bindings[$id] = array_merge([
+            'concrete' => Closure::bind(function() use ($id) { return $id; }, null),
+            'shared' => false,
+            'parameters' => [],
+        ], $configuration);
+
+        return $this;
+    }
+
+    public function get(string $id)
+    {
+        return $this->resolve($id);
+    }
+
+    public function has(string $id): bool
+    {
+        return (bool) $this->getBinding($id);
+    }
+
+    public function resolve(string $id)
+    {
+        if (isset($this->sharedInstances[$id])) {
+            return $this->sharedInstances[$id];
+        }
+
+        $ret = $this->getClosure($id)($this);
+
+        if ($this->isShared($id)) {
+            $this->sharedInstances[$id] = $ret;
+        }
+
+        return $ret;
+    }
+
+    protected function getClosure(string $id)
+    {
+        $binding = $this->getBinding($id);
+
+        if (!$binding) {
+            $binding = [
+                'concrete' => $id,
+                'parameters' => []
+            ];
+        }
+
+        if ($binding['concrete'] instanceof Closure) {
+            return $binding['concrete'];
+        }
+
+        try {
+            $reflectorClass = new ReflectionClass($binding['concrete']);
+        } catch (ReflectionException $exc) {
+            return function () use ($binding) {
+                return new Exception("Class {$binding['concrete']} does not exist");
+            };
+        }
+
+        if (!$reflectorClass->isInstantiable()) {
+            return function () use ($binding) {
+                return new Exception("Cannot instantiate {$binding['concrete']}.");
+            };
+        }
+
+        $constructor = $reflectorClass->getConstructor();
+        $parameters = $constructor ? $this->getParams($constructor, $binding) : [];
+
+        if (count($parameters)) {
+            return function() use ($reflectorClass, $parameters) {
+                return new $reflectorClass->name(...$parameters);
+            };
+        } else {
+            return function () use ($reflectorClass) {
+                return new $reflectorClass->name;
+            };
+        }
+    }
+
+    public function getParams(ReflectionMethod $method, array $binding): array
+    {
+        $resolvedParams = [];
+
+    	foreach ($method->getParameters() as $index => $parameter) {
+            if (array_key_exists($index, $binding['parameters']) || array_key_exists($parameter->getName(), $binding['parameters'])) {
+                $resolvedParam = $binding['parameters'][$index] ?? $binding['parameters'][$parameter->getName()];
+            } else {
+    			$type = $parameter->getType();
+                $isBuiltIn = $type instanceof \ReflectionNamedType && $type->isBuiltIn();
+                if ($isBuiltIn) {
+                    $resolvedParam = $parameter->isDefaultValueAvailable() ? $parameter->getDefaultValue() : null;
+                } else {
+                    $resolvedParam = $this->resolve($type->getName());
+                }
+            }
+
+            if ($parameter->isVariadic()) {
+                $resolvedParams[] = [$result];
+                break;
+            } else {
+                $resolvedParams[] = $resolvedParam;
+            }
+		}
+
+        return $resolvedParams;
+    }
+}
---
+++ test/SimpleDITest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'vendor/autoload.php';
+require_once 'Basic.php';
+
+class SimpleDITest extends \PHPUnit\Framework\TestCase {
+	protected $container;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->container = new \Otobio\SimpleDI();
+	}
+
+	protected function tearDown(): void {
+		$this->container = null;
+		parent::tearDown();
+	}
+
+    public function testBasicWithoutConcrete()
+    {
+        $this->container->add('basic');
+        $this->assertEquals($this->container->get('basic'), 'basic');
+    }
+
+    public function testBasicWithCallableConcrete()
+    {
+        $this->container->add('basic', [
+            'concrete' => function () {
+                return 'Hello';
+            }
+        ]);
+        $this->assertEquals($this->container->get('basic'), 'Hello');
+    }
+
+    public function testBasicWithReferencedConcrete()
+    {
+        $this->container->add('basic', [
+            'concrete' => function () {
+                return 'Hello';
+            }
+        ])->add('simple', [
+            'concrete' => 'basic'
+        ]);
+
+        $this->assertEquals($this->container->get('simple'), 'Hello');
+    }
+
+    public function testBasicClassWithNoConstructor()
+    {
+        $this->container->add('basic', [
+            'concrete' => NoConstructor::class
+        ]);
+
+        $this->assertInstanceOf(NoConstructor::class, $this->container->get('basic'));
+    }
+
+    public function testContainerConfigShareBehaviour()
+    {
+        $this->container->add(InterfaceTest::class, [
+            'concrete' => InterfaceTestClass::class,
+        ]);
+
+        $this->assertNotSame($this->container->get(InterfaceTest::class), $this->container->get(InterfaceTest::class));
+
+        $this->container->add(InterfaceTest::class, [
+            'concrete' => InterfaceTestClass::class,
+            'shared' => true
+        ]);
+
+        $this->assertSame($this->container->get(InterfaceTest::class), $this->container->get(InterfaceTest::class));
+    }
+
+    public function testReflectionClassWithNoConstructor()
+    {
+        $this->assertInstanceOf(NoConstructor::class, $this->container->get(NoConstructor::class));
+    }
+
+	public function testObjectGraphCreation() {
+		$a = $this->container->get('A');
+
+		$this->assertInstanceOf('B', $a->b);
+		$this->assertInstanceOf('c', $a->b->c);
+		$this->assertInstanceOf('D', $a->b->c->d);
+		$this->assertInstanceOf('E', $a->b->c->e);
+		$this->assertInstanceOf('F', $a->b->c->e->f);
+	}
+
+}
